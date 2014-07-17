968bcb1525866f8d95b35315ba05fdf45ef30425
Diane Douglas & Catherine Anh Tran <student@epicodus.com>
2014-07-16 17:01:20 -0700

    added html layout and click event handler on add-address span.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                '<div class="form-group">' +
+                  '<label for="new-street">Street</label>' +
+                  '<input type="text" class="form-control new-street">' +
+                '</div>' +
+                '<div class="form-group">' +
+                  '<label for="new-city">City</label>' +
+                  '<input type="text" class="form-control new-city">' +
+                '</div>' +
+                '<div class="form-group">' +
+                  '<label for="new-state">State</label>' +
+                  '<input type="text" class="form-control new-state">' +
+                '</div>' +
+              '</div>');
+  })
+
+
+})
