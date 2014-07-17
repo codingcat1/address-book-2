cfb0534fd0ce1ae17a7fab5f9bd3cf9d342a878a
Diane Douglas & Catherine Anh Tran <student@epicodus.com>
2014-07-17 11:54:49 -0700

    Hooked up all the jquery for showing multiple addresses, phone numbers and emails.

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,80 @@ $(document).ready(function() {
                 '</div>' +
               '</div>');
   })
+  $("#add-phone").click(function(){
+    $("#new-phones").append('<div class="new-phone-section">'+
+                '<div class="form-group">'+
+                  '<label for="new-phone">Phone</label>'+
+                  '<input type="text" class="form-control new-phone">'+
+                '</div>'+
+              '</div>');
 
+  })
+  $("#add-email").click(function(){
+    $("#new-emails").append('<div class="new-email-section">'+
+                '<div class="form-group">'+
+                  '<label for="new-email">Email</label>'+
+                  '<input type="text" class="form-control new-email">'+
+                '</div>'+
+              '</div>');
 
-})
+  })
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [], phones: [], emails: [] };
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+
+      var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+      newContact.addresses.push(newAddress);
+    });
+
+    $('.new-phone-section').each(function(){
+      var inputtedPhone = $(this).find("input.new-phone").val();
+      newContact.phones.push(inputtedPhone);
+    });
+
+    $('.new-email-section').each(function(){
+      var inputtedEmail = $(this).find("input.new-email").val();
+      newContact.emails.push(inputtedEmail);
+    });
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+      });
+      $("ul#phonenumbers").text("");
+      newContact.phones.forEach(function(phone) {
+        $("ul#phonenumbers").append("<li>" + phone + "</li>");
+      });
+      $("ul#emails").text("");
+      newContact.emails.forEach(function(email) {
+        $("ul#emails").append("<li>" + email + "</li>");
+      });
+    });
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+    $("input.new-phone").val("");
+    $("input.new-email").val("");
+
+  });
+});
